046282835cd858f8cbbf889e37151d5aea5527b0
Akshat Agarwal <akshatagarwal@Akshats-MacBook-Air.local>
2023-11-01 20:32:15 +0530

    Animated text added

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -19,32 +20,45 @@ class _WelcomeScreenState extends State<WelcomeScreen>
     super.initState();
     controller = AnimationController(
       duration: Duration(
-        seconds: 1,
+        seconds: 3,
       ),
       vsync: this,
       upperBound: 1.0,
     );
-    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
-    controller.reverse(from: 1.0);
+
+    // animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
+
     controller.forward();
+
+    // animation.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     controller.reverse(from: 1.0);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     controller.forward();
+    //   }
+
     controller.addListener(() {
       setState(() {});
-      print(controller.value);
+      print(animation.value);
     });
+
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red.withOpacity(controller.value.toDouble()),
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
+          children: [
             Row(
-              children: <Widget>[
+              children: [
                 Hero(
                   tag: 'logo',
                   child: Container(
@@ -52,15 +66,19 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                     height: controller.value * 100,
                   ),
                 ),
-                Text(
-                  // '${controller.value.toInt()}%',
-                  "Flash Chat",
-                  style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 35.0,
-                    fontWeight: FontWeight.w900,
-                  ),
-                ),
+                AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      "Flash Chat",
+                      textStyle: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.w900,
+                      ),
+                      speed: Duration(milliseconds: 200),
+                    ),
+                  ],
+                )
               ],
             ),
             SizedBox(
